741a2eff74ca3072ee554619f383fd4cd46f2912
Kiran Adhikari <kiranadh1452@gmail.com>
2022-11-07 18:33:51 +0545

    Stripe code alteration

--- stripe.js
+++ stripe.js
@@ -5,28 +5,57 @@ require("dotenv").config();
 // create a stripe instance
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
-exports.listAllProducts = async (limit = undefined) => {
+/**
+ * description: list all products available in stripe
+ * @param {number} limit - number of products to retrieve
+ * @returns {object} products
+ */
+exports.retrieveAllProducts = async (limit = undefined) => {
     try {
         const products = await stripe.products.list({
             limit,
         });
-        console.log(products);
         return products;
     } catch (error) {
         console.log(error);
     }
 };
 
+/**
+ * description: retrieve a product by id
+ * @param {string} productId - id of the product
+ * @returns {object} product
+ */
 exports.retrieveProduct = async (productId) => {
     try {
         const product = await stripe.products.retrieve(productId);
-        console.log(product);
         return product;
     } catch (error) {
         console.log(error);
     }
 };
 
+/**
+ * description: Retrieve all prices
+ * @param {number} limit - number of prices to retrieve
+ * @returns {object} prices
+ */
+exports.retrieveAllPrice = async (limit = undefined) => {
+    try {
+        const prices = await stripe.prices.list({
+            limit,
+        });
+        return prices;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+/**
+ * description: retrieve a price by id
+ * @param {string} priceId - id of the price
+ * @returns {object} price
+ */
 exports.retrievePrice = async (priceId) => {
     try {
         const price = await stripe.prices.retrieve(priceId);
