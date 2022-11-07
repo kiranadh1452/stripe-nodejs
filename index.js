3fa7e2bb0515f65103cd126d118b953150b91878
Kiran Adhikari <kiranadh1452@gmail.com>
2022-11-07 18:58:21 +0545

    Index file removed

--- index.js
+++
@@ -1,5 +0,0 @@
-const stripeHandler = require("./stripe");
-
-// stripeHandler.listAllProducts();
-// stripeHandler.retrieveProduct("prod_Mjxyvzjybddx38");
-stripeHandler.retrievePrice("price_1M0U2EIj94rrl9qiwQPw9aCt");
